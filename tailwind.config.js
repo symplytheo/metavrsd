f3b79e670425d55dbc98780ab7f89ade67ee4e00
Theo Iyonor <symplytheo@gmail.com>
2021-11-18 18:03:56 +0100

    lint setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#00aca5',
+          DEFAULT: '#00f7ed',
+          dark: '#33f8f0',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
