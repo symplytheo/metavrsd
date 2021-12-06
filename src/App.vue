f9e9c1625fe384148da0e36dcb312c358254a984
Theo Iyonor <symplytheo@gmail.com>
2021-12-06 13:03:47 +0100

    [FIX] correct image orientation and text font-size

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     </a>
   </header>
 
-  <main class="pt-2">
+  <main>
     <section
       class="
         container
@@ -16,16 +16,14 @@
         md:h-screen md:mx-0 md:max-w-full md:text-left
       "
     >
-      <div class="container max-w-screen-md mx-auto relative">
-        <h2
-          class="text-5xl sm:text-6xl md:text-6xl md:text-center text-primary"
-        >
+      <div class="container max-w-screen-md pt-8 mx-auto relative">
+        <h2 class="text-5xl sm:text-6xl md:text-center text-primary">
           welcome,
         </h2>
-        <h3 class="text-5xl sm:text-6xl md:text-6xl tracking-wider">
+        <h3 class="text-5xl sm:text-6xl mb-1 tracking-wider">
           let’s get you meta-versed
         </h3>
-        <div class="mt-3 mb-5 md:text-xl hidden md:block font-light">
+        <div class="mt-1 mb-4 md:text-lg hidden md:block font-light">
           the world is changing really fast and it’s easy to feel like <br />
           you can’t keep up with everything. <br />
           <span class="font-medium">well, here we are...</span>
@@ -35,7 +33,7 @@
             <base-button
               size="large"
               variant="outlined"
-              class="w-full md:w-auto border-primary"
+              class="w-4/5 md:w-auto border-primary"
             >
               Join our community
             </base-button>
@@ -45,7 +43,7 @@
               size="large"
               variant="outlined"
               color="white"
-              class="w-full md:w-auto"
+              class="w-4/5 md:w-auto"
             >
               Partner with us
             </base-button>
@@ -53,7 +51,7 @@
         </div>
 
         <div
-          class="absolute -top-32 md:left-0 w-full hidden md:block"
+          class="absolute -top-28 md:left-0 w-full hidden md:block"
           style="z-index: -1"
         >
           <img
@@ -90,7 +88,9 @@
           lg:max-w-screen-sm lg:pl-40 lg:mx-auto
         "
       >
-        <h4 class="text-5xl font-bold md:hidden">Our Mission</h4>
+        <h4 class="text-5xl font-bold md:hidden tracking-widest">
+          our mission
+        </h4>
         <p class="text-2xl md:pt-16">
           to educate the world about the <br />
           <span class="text-primary inline-block">
@@ -119,7 +119,7 @@
         our &nbsp;mission
       </div>
 
-      <div class="absolute -top-64 left-0 hidden md:block" style="z-index: -1">
+      <div class="absolute -top-60 left-0 hidden md:block" style="z-index: -1">
         <img src="./assets/rock-large.png" class="w-96 object-contain" />
       </div>
     </section>
@@ -157,12 +157,17 @@
     </section>
   </main>
 
-  <div class="relative -mt-20 overflow-hidden">
+  <footer class="relative mt-20 md:-mt-20">
     <img
       src="./assets/okay-large.png"
-      class="mx-auto w-2/3 object-contain relative"
+      class="hidden md:block mx-auto w-2/3 object-contain relative"
     />
-    <div class="absolute bottom-0 left-0 w-full h-3/5 hidden md:block">
+    <img
+      src="./assets/okay.png"
+      class="md:hidden w-full sm:w-3/4 sm:mx-auto relative"
+    />
+
+    <div class="absolute bottom-0 left-0 w-full h-3/4 py-7 md:py-1 md:h-3/5">
       <div
         class="
           container
@@ -204,20 +209,19 @@
         </form>
 
         <div id="partner" class="md:pt-14 md:pb-6 lg:pt-28 pb-12">
-          <h5 class="text-4xl md:text-2xl font-light">
+          <h5 class="text-4xl sm:text-5xl md:text-2xl font-light">
             want to
-            <span class="font-normal"
-              >work or <br class="md:hidden" />
-              partner</span
-            >
+            <span class="font-normal block md:inline">work or partner</span>
             with us?
           </h5>
-          <p class="font-bold text-lg">hello@metavrsd.io</p>
+          <p class="font-bold text-2xl md:text-lg">hello@metavrsd.io</p>
         </div>
 
         <div id="community" class="md:py-8 lg:py-16">
-          <h5 class="text-2xl font-light">join our community</h5>
-          <p class="text-sm">
+          <h5 class="text-3xl sm:text-4xl md:text-2xl font-light">
+            join our community
+          </h5>
+          <p class="text-sm sm:text-base md:text-sm">
             <span v-for="(item, i) in socials" :key="i">
               <a
                 :href="item.href"
@@ -232,7 +236,7 @@
           </p>
         </div>
 
-        <div class="uppercase font-light text-sm tracking-widest">
+        <div class="uppercase mt-24 md:mt-0 font-light text-sm tracking-widest">
           San Francisco | Lagos | Waterloo
         </div>
 
@@ -244,82 +248,7 @@
         </div>
       </div>
     </div>
-  </div>
-
-  <!-- <footer
-    id="footer"
-    class="flex flex-col justify-end items-end pt-16 md:pt-60"
-  >
-    <div class="container max-w-screen-sm mx-auto">
-      <form
-        @submit.prevent="joinWaitlist"
-        class="hidden md:block max-w-md mx-auto mt-10 mt:0 text-center"
-      >
-        <h5 class="text-xl font-bold mb-3">
-          be the first to know when we launch
-        </h5>
-        <div class="px-3">
-          <input
-            v-model="email"
-            type="email"
-            placeholder="Enter your email"
-            class="
-              px-3
-              py-3
-              placeholder-gray-200
-              bg-transparent
-              text-sm
-              border border-white
-              outline-none
-              focus:outline-none focus:ring-0
-              w-full
-            "
-            required
-          />
-        </div>
-      </form>
-
-      <div
-        id="partner"
-        class="pt-32 pb-8 mt-8 px-5 text-center md:pt-10 my-10 md:my-0"
-      >
-        <h5 class="text-4xl md:text-xl">
-          want to work or <br class="md:hidden" />
-          partner with us?
-        </h5>
-        <p class="font-bold">hello@metavrsd.io</p>
-      </div>
-
-      <div id="community" class="pt-8 pb-32 md:pb-12 text-center">
-        <h5 class="text-2xl">join our community</h5>
-        <p class="font-bold text-sm">
-          <span v-for="(item, i) in socials" :key="i">
-            <a
-              :href="item.href"
-              class="m-1"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {{ item.title }}
-            </a>
-            <span v-if="i < socials.length - 1">-</span>
-          </span>
-        </p>
-      </div>
-
-      <div class="pb-4 hidden md:block text-sm uppercase text-center">
-        San Francisco | Lagos | Waterloo
-      </div>
-
-      <div class="text-center p-2 md:pb-3">
-        <div class="mb-2 md:hidden">San Francisco | Lagos | Waterloo</div>
-        <div class="text-black font-bold">
-          &copy; <a href="/" class="inline-flex">METAVRSD</a>
-          {{ new Date().getFullYear() }}.
-        </div>
-      </div>
-    </div>
-  </footer> -->
+  </footer>
 </template>
 
 <script>
@@ -348,19 +277,4 @@ export default {
 }
 </script>
 
-<style>
-#footer {
-  background-image: url('./assets/okay.png');
-  background-size: 100% 732px;
-  background-repeat: no-repeat;
-  background-position: center bottom -100px;
-}
-
-@media (min-width: 768px) {
-  #footer {
-    background-image: url('./assets/okay-large.png');
-    background-size: 60% 100vh;
-    /* background-position: center bottom -250px; */
-  }
-}
-</style>
+<style></style>
